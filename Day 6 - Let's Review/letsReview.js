1782c0bc724a3dc44b3cf7dad8566bacd2e2170c
Moeen Mahmud <write2fahim007@gmail.com>
2022-08-07 13:45:15 +0600

    added day 6

---
+++ Day 6 - Let's Review/letsReview.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+	inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+	inputString = inputString.split('\n');
+
+	main();
+});
+
+function readLine() {
+	return inputString[currentLine++];
+}
+
+function multiplicationTable(input) {
+	let result = 1;
+	for (let i = 1; i <= 10; i++) {
+		result = input * i;
+		console.log(`${input} x ${i} = ${result}`);
+	}
+}
+
+function main() {
+	const n = parseInt(readLine().trim(), 10);
+	multiplicationTable(n);
+}
