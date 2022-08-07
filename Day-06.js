08fbf6e95510f9b4d0903c0993ff70739a57785e
Moeen Mahmud <write2fahim007@gmail.com>
2022-08-07 11:52:33 +0600

    added Day 0: Hello World

---
+++ Day-06.js
@@ -0,0 +1,30 @@
+function processData(input) {
+	let message = input.split("\n");
+	let processedNumber = parseInt(message[0]);
+
+	for(let i=0; i<processedNumber; i++) {
+		let processedMessage = message[i + 1];
+		let processedMessageLen = processedMessage.length;
+		let messageArray = processedMessage.split("");
+		let evenString = "";
+		let oddString = "";
+		for(let j = 0; j < processedMessageLen; j++) {
+			if(j % 2 === 0) {
+				evenString += messageArray[j]
+			}else {
+				oddString += messageArray[j]
+			}
+		}
+		console.log(evenString + " " + oddString);
+	}
+}
+
+
+
+let string = "2 " +
+	"Hacker" +
+	"Rank";
+// console.log(processSpace(2));
+
+processData(string);
+// console.log(string.slice(1));
